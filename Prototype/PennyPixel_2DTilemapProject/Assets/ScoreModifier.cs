8065942982d693748970e72a79e422ced00b9b5f
M1sterChaos <austinbuck70@gmail.com>
2022-02-26 16:44:02 -0600

    Finished Prototype

---
+++ Prototype/PennyPixel_2DTilemapProject/Assets/ScoreModifier.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * (Austin Buck)
+ * (Assignment 5)
+ * (Controls the score)
+ */
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreModifier : MonoBehaviour
+{
+    public static int score = 0;
+    public Text scoreText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = "Gems Collected: " + score;
+
+        if(score >= 10)
+        {
+            scoreText.text = "You collected all the gems! \nGo to the end zone!";
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            score++;
+        }
+    }
+}
---
+++ Prototype/PennyPixel_2DTilemapProject/Assets/Win.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * (Austin Buck)
+ * (Assignment 5)
+ * (Controls the win text)
+ */
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Win : MonoBehaviour
+{
+    public Text WinText;
+
+    public GameObject wto;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (ScoreModifier.score >= 10 && collision.gameObject.tag == "Player")
+        {
+            wto.SetActive(true);
+            WinText.text = "You've Won!";
+        }
+    }
+}
